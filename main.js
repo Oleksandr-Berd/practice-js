f07c96df5181edfa7695972fe9fdb385f025300c
Oleksandr-Berd <alex.berd86@gmail.com>
2024-08-22 16:09:12 +0300

    keep on

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const min =  (list) => {
-  return Math.min(...list);
+// Sum Numbers
+const rps = (p1, p2) => {
+
+  if (p1 === "scissors" && p2 === "rock") {
+  return "Player 2 won!"
+  } else if (p1 === "scissors" && p2 === "paper") {
+    return "Player 1 won!"
+  } else if (p1 === p2) {
+    return "Draw!"
+} 
+
 };
 
-const max =  (list) => {
-  return Math.max(...list);
-};
-
-console.log(min([1, 2, 3, 4, 5, 6, 7, 8, 9]), max([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+console.log(sum([]));
